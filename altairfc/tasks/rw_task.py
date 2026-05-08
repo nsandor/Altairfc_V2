6dded9fc234b53553aa106e7f9242cde101d18a4
bdagnillo <bdagnillo@gmail.com>
2026-05-08 12:54:22 -0400

    working setting alt for motor

--- altairfc/tasks/rw_task.py
+++ altairfc/tasks/rw_task.py
@@ -90,8 +90,8 @@ class RWTask(BaseTask):
         quat, pos, gs_pos, yaw_rate, yaw = self._read()
         az_err, _ = compute_error(quat, pos, gs_coords=gs_pos)
         self._store()
-        control_signal = self.controller.output(yaw, yaw_rate) + 1700.0
-        logger.info("yaw_error:%f, control signal: %f", az_err, control_signal)
+        control_signal = self.controller.output(yaw, yaw_rate) + 2150.0
+        logger.info("yaw_error: %f, yaw_rate: %f, control signal: %f", yaw, yaw_rate, control_signal)
         self.motor.set_rpm(int(control_signal))
 
 
