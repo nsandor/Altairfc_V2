cae1332479a172e6b916e33f436a3cfc21bc2e4a
Yorgo <abouhaidar.yorgo@gmail.com>
2026-05-24 18:49:24 -0400

    tuning

--- altairfc/drivers/rw_driver.py
+++ altairfc/drivers/rw_driver.py
@@ -11,7 +11,7 @@ class RWDriver:
         self.motor: VESCObject | None = None
         self.connected = False
         self._last_rpm = 0
-        self.max_delta_rpm = 30
+        self.max_delta_rpm = 100
 
     def connect(self) -> bool:
         try:
