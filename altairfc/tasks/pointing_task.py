7abd66761b7e9307fe45cc6d18a679f263e5d004
Yorgo <abouhaidar.yorgo@gmail.com>
2026-05-25 21:25:57 -0400

    Stabilization logic

--- altairfc/tasks/pointing_task.py
+++ altairfc/tasks/pointing_task.py
@@ -62,6 +62,7 @@ class PointingTask(BaseTask):
         self._target_offset = 0.0
         self._count = 0
         self._hold_count = 0
+        self.j = 0.0
 
         if self.rw is not None:
             if not self.rw.connect():
@@ -91,13 +92,18 @@ class PointingTask(BaseTask):
             self._check()
             self.rw_controller.set_mode("stabilize")
             _, _, _, yaw_rate, _, rw_rpm = self._read()
-            if abs(yaw_rate) < self._stabilize_yaw_rate and abs(rw_rpm) < self._saturation_rpm:
-                self._set_state(PointingState.POINTING)
-            if self._is_saturated(rw_rpm):
-                self._set_state(PointingState.SATURATED)
-                return
+            saturated = self._is_saturated(rw_rpm)
+            trend = self._acceleration(yaw_rate)
+            if abs(yaw_rate) >= self._stabilize_yaw_rate or time.monotonic() - self._state_started < 10.0:
+                if np.sign(trend) != np.sign(yaw_rate):
+                    delta_rpm = self.rw_controller.output(yaw_rate)
+                elif saturated:
+                    delta_rpm = 0.0
+                else:
+                    delta_rpm = self.rw_controller.output(yaw_rate)
             else:
-                delta_rpm = self.rw_controller.output(yaw_rate)
+                self._set_state(PointingState.POINTING)
+                delta_rpm = 0.0
             
             self.rw.set_rpm(int(rw_rpm + delta_rpm))
 
