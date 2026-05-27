496c02f64aabc4d61eadaa8b2af27d915cab8960
Yorgo <abouhaidar.yorgo@gmail.com>
2026-05-27 10:02:51 -0400

    Changing setpoint to 1500 because of vibrations

--- altairfc/tasks/pointing_task.py
+++ altairfc/tasks/pointing_task.py
@@ -114,7 +114,7 @@ class PointingTask(BaseTask):
             self._set_state(PointingState.STABILIZE)
             return
         self.datastore.write("pointing.heading_error", err)
-        rpm_cmd = self.rw_controller.output(err, yaw_rate) + 1750
+        rpm_cmd = self.rw_controller.output(err, yaw_rate) + 1500
 
         self.rw.set_rpm(int(rpm_cmd))
 
@@ -186,9 +186,9 @@ class PointingTask(BaseTask):
         return (now - self._unstable_since) >= self._stability_threshold
 
     def _desaturate(self) -> None:
-        self.rw.decelerate(1750)
+        self.rw.decelerate(1500)
         _, _, _, _, _, rw_rpm, hdg = self._read()
-        if time.monotonic() - self._state_started >= 5.0 and 1650 < abs(rw_rpm) < 1850:
+        if time.monotonic() - self._state_started >= 5.0 and 1400 < abs(rw_rpm) < 1600:
             self._set_state(PointingState.STABILIZE)
     
     def _stabilize(self) -> None:
@@ -210,7 +210,7 @@ class PointingTask(BaseTask):
             self._set_state(PointingState.POINTING)
             return
 
-        rw_cmd = self.rw_controller.output(yaw_rate) + 1750
+        rw_cmd = self.rw_controller.output(yaw_rate) + 1500
         self.rw.set_rpm(int(rw_cmd))
 
     
